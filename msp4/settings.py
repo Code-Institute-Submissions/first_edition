638ad15e225e608dfa35b68be44c45b8d191ee4c
conor52 <conormccarthy157@gmail.com>
2020-12-15 14:06:03 +0000

    moved secret key to heroku and changed debug

--- msp4/settings.py
+++ msp4/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'q4sopmn&%^^o@&45_4*7(e_4x(*rm!c5rmb*707^&n=%x+udyd'
+SECRET_KEY = os.environ.get("SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = "DEVELOPMENT" in os.environ
 
 ALLOWED_HOSTS = ["first-edition.herokuapp.com", "localhost"]
 
