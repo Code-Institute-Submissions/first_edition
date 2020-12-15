81cd9f0d334ad2f31a8408f236113c32b466d23c
conor52 <conormccarthy157@gmail.com>
2020-12-15 13:04:03 +0000

    tweaking of settings.py for deployment

--- msp4/settings.py
+++ msp4/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'q4sopmn&%^^o@&45_4*7(e_4x(*rm!c5rmb*707^&n=%x+udyd'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["first-edition.herokuapp.com", "localhost"]
 
 
 # Application definition
@@ -118,15 +118,20 @@ LOGIN_REDIRECT_URL = '/'
 WSGI_APPLICATION = 'msp4.wsgi.application'
 
 
-Database
-https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
