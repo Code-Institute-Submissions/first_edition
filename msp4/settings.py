6e46d50087f995a8b58a6f520e3289282ff31b91
conor52 <conormccarthy157@gmail.com>
2020-12-16 10:26:42 +0000

    deployment testing

--- msp4/settings.py
+++ msp4/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY", "")
+SECRET_KEY = "=e%iscz(do$ct874-po1d0dtc3!ecps$tp47vpy44o!$qpy(s4"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "DEVELOPMENT" in os.environ
