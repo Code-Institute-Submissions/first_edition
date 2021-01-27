a37d619b46e90ca94121648d31c062a8b1797e8a
conor52 <conormccarthy157@gmail.com>
2021-01-27 11:17:46 +0000

    improved viewwidth on index page

--- checkout/views.py
+++ checkout/views.py
@@ -34,6 +34,9 @@ def cache_checkout_data(request):
 
 
 def checkout(request):
+    """ The checkout app creates the payment intent with the help of stripe keys,
+     handles the post data on the checkout template and
+      allows for loading of pre existanting delivery info """
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
@@ -41,6 +44,7 @@ def checkout(request):
         bag = request.session.get('bag', {})
 
         form_data = {
+            # this is done manually to avoid posting the save info box
             'full_name': request.POST['full_name'],
             'email': request.POST['email'],
             'phone_number': request.POST['phone_number'],
@@ -61,22 +65,13 @@ def checkout(request):
             for item_id, item_data in bag.items():
                 try:
                     product = Product.objects.get(id=item_id)
-                    if isinstance(item_data, int):
-                        order_line_item = OrderLineItem(
-                            order=order,
-                            product=product,
-                            quantity=item_data,
+                    order_line_item = OrderLineItem(
+                        order=order,
+                        product=product,
+                        quantity=item_data,
                         )
-                        order_line_item.save()
-                    else:
-                        for size, quantity in item_data(
-                             ['items_by_size'].items()):
-                            order_line_item = OrderLineItem(
-                                order=order,
-                                product=product,
-                                quantity=quantity,
-                            )
-                            order_line_item.save()
+                    order_line_item.save()
+
                 except Product.DoesNotExist:
                     messages.error(request, (
                         "One of the products in your"
@@ -94,6 +89,7 @@ def checkout(request):
                 Please double check your information.')
     else:
         bag = request.session.get('bag', {})
+        # prevents user from typing/checkout in url
         if not bag:
             messages.error(
                 request, "There's nothing in your bag at the moment")
@@ -101,6 +97,7 @@ def checkout(request):
 
         current_bag = bag_contents(request)
         total = current_bag['grand_total']
+        # Stripe requires an int to make a charge, so I rounded it out
         stripe_total = round(total * 100)
         stripe.api_key = stripe_secret_key
         intent = stripe.PaymentIntent.create(
@@ -142,6 +139,10 @@ def checkout(request):
 
 
 def checkout_success(request, order_number):
+    """ Takes the newly made order from checkout and renders
+     it out for the user who just successfully purchased a product.
+      It's also linked to their profile here, if they have one.
+       The bag is now deletes """
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
 
