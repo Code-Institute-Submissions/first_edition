9b7ec072a5a0eaefefd0eb5893cb8b8c0cfbd18c
conor52 <conormccarthy157@gmail.com>
2021-01-30 12:34:42 +0000

    removed edit and del comment urls

--- products/urls.py
+++ products/urls.py
@@ -14,14 +14,6 @@ urlpatterns = [
     path(
         'view_comments/'
         '<int:product_id>/', views.view_comments, name='view_comments'),
-    path(
-        'edit_comment/'
-        '<int:product_id>/', views.edit_comment, name='edit_comment'),
-    path(
-        'delete_comment/'
-        '<int:product_id>/', views.delete_comment, name='delete_comment'),
-    path('rate_product/<int:product_id>/', views.rate, name='rate'),
-
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
     path('delete/<int:product_id>/', (
         views.delete_product), name='delete_product'),
